112fdb2e08898afdca99a99894b7867b9e91fecd
ou786 <89064800+ou786@users.noreply.github.com>
2023-06-18 10:35:59 +0530

    File

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StudentRegistrationComponent } from './student-registration/student-registration.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentRegistrationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/student-registration/student-registration.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-student-registration',
+  templateUrl: './student-registration.component.html',
+  styleUrls: ['./student-registration.component.css']
+})
+export class StudentRegistrationComponent {
+  student = {
+    name: '',
+    dob: '',
+    gender: ''
+    // Add more properties for other student information
+  };
+
+  students: any[] = [];
+
+  onSubmit() {
+    this.students.push({ ...this.student });
+    this.student = {
+      name: '',
+      dob: '',
+      gender: ''
+      // Reset other student properties
+    };
+  }
+}
